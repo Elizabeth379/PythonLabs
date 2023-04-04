f9630eb2407fbfc4018057b0d5f0ed888940c87c
Elizabeth379 <bellator.379@gmail.com>
2023-04-04 23:09:21 +0300

    Amount of sentences functions added

--- Lab2/Task1/main.py
+++ Lab2/Task1/main.py
@@ -2,21 +2,30 @@ import re
 
 EXCLUSIONS = ["Dr", "Mr", "Mrs","Lt", "Rep", "etc"]
 
+def amount_of_non_declarative_sentences(text):
+    non_declarative_pattern = r'([0-9]+[a-zA-Z]+\?)|([a-zA-Z]+\?)|([a-zA-Z]+[0-9]+\?)|([0-9]+\?)|([0-9]+[a-zA-Z]+\!)|([a-zA-Z]+\!)|([a-zA-Z]+[0-9]+\!)|([0-9]+\!)'
+    end_non_dec_match = re.findall(non_declarative_pattern, text)
+    sentences_with_non_dec = len(end_non_dec_match)
+    return sentences_with_non_dec
+
+def amount_of_sentences(text, sentences_with_non_dec):
+    point_pattern = r'([0-9]+[a-zA-Z]+\.)|([a-zA-Z]+\.)|([a-zA-Z]+[0-9]+\.)|([0-9]+\.)'
+    end_point_match = re.findall(point_pattern, text)
+    sentences_with_poits = len(end_point_match)
+    sentences_in_the_text = sentences_with_poits + sentences_with_non_dec
+    return sentences_in_the_text
+
 
 if __name__=='__main__':
-    point_pattern = r'([0-9]+[a-zA-Z]+\.)|([a-zA-Z]+\.)|([a-zA-Z]+[0-9]+\.)|([0-9]+\.)'
-    non_declarative_pattern = r'([0-9]+[a-zA-Z]+\?)|([a-zA-Z]+\?)|([a-zA-Z]+[0-9]+\?)|([0-9]+\?)|([0-9]+[a-zA-Z]+\!)|([a-zA-Z]+\!)|([a-zA-Z]+[0-9]+\!)|([0-9]+\!)'
 
     text = input("Write your text:")
-    sentences_with_poits = 0
 
-    for end_point_match in re.findall(point_pattern, text):
-        sentences_with_poits = sentences_with_poits + 1
-        #print(end_point_match)
+    sentences_with_non_dec = amount_of_non_declarative_sentences(text)
+    sentences_in_the_text = amount_of_sentences(text, sentences_with_non_dec)
 
-
-
-    #print(re.findall(point_pattern, text))
-    print(sentences_with_poits)
+    print("Amount of non-declarative sentences in the text:")
+    print(sentences_with_non_dec)
+    print("Amount of sentences in the text:")
+    print(sentences_in_the_text)
 
 
