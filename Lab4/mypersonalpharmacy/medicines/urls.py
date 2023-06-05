05e016bb9e700ead91e4cd9b2a47ac08ffcde64b
Elizabeth379 <bellator.379@gmail.com>
2023-06-06 00:44:54 +0300

    Base list template html created

--- Lab4/mypersonalpharmacy/medicines/urls.py
+++ Lab4/mypersonalpharmacy/medicines/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', test, name = 'medhome'),  # http://127.0.0.1:8000/medicines/
-    path('medlist/<int:medid>/', mlist),  # http://127.0.0.1:8000/medicines/medlist/1/
+    path('about/', about, name='about'),  # http://127.0.0.1:8000/medicines/about/
+    path('medlist/', mlist),  # http://127.0.0.1:8000/medicines/medlist/
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),  # http://127.0.0.1:8000/medicines/archive/1999/
 
 ]
--- Lab4/mypersonalpharmacy/medicines/views.py
+++ Lab4/mypersonalpharmacy/medicines/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = ["О нас", "Список лекарств", "Войти"]
+menu1 = ["Вы знаете", "Что этот сайт", "Сделан для Вас :3"]
+
 
 def test(request):
-    return HttpResponse("Страница приложения medicines.")
+    return render(request, 'medicines/test.html', {'menu': menu, 'title': 'Домашняя страница'})
 
 
-def mlist(request, medid):
+def about(request):
+    return render(request, 'medicines/about.html', {'menu': menu1, 'title': 'О нас'})
+
+
+def mlist(request):
+    posts = Medicines.objects.all()
     if request.GET:
-        print(request.GET)  # http://127.0.0.1:8000/medicines/medlist/1/?name=George&type=medic
+        print(request.GET)  # http://127.0.0.1:8000/medicines/medlist/?name=George&type=medic
     if request.POST:
         print(request.POST)
-    return HttpResponse(f"<h1>Список лекарств</h1><p>{medid}</p>")
+    return render(request, 'medicines/med_list.html', {'posts': posts, 'menu': menu1, 'title': 'Лекарства'})
 
 
 def archive(request, year):
