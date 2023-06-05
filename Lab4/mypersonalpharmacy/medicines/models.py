79aede757d359132b5c927736054470fee17c601
Elizabeth379 <bellator.379@gmail.com>
2023-06-05 20:27:06 +0300

    First info added to db. Model Medicine created

--- Lab4/mypersonalpharmacy/medicines/models.py
+++ Lab4/mypersonalpharmacy/medicines/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
 class Medicines(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%n/%d/")
 
     is_available = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Medicine'
+        verbose_name_plural = 'Medicines'
+
+    def __str__(self):
+        return self.title
